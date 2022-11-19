ec5561c457a809eeeee344a1a41ab0048fca6949
Daxer3000 <36710604+Daxer3000@users.noreply.github.com>
2022-11-19 11:12:40 +0100

    Update Mathe-B-Tag_A7_b.py

--- Mathe-B-Tag_A7_b.py
+++ Mathe-B-Tag_A7_b.py
@@ -1,58 +1,54 @@
-
 ###  Da Pi irratioal ist, ist nur eine annährungsweise Kalkulation zur Zahl 1 hin möglich. ###
 
 
 from math import pi
-def get_all_numbers(volume_n = 5, volume_m = pi):
-	list_m = [0, volume_m]
-	list_n = [0]
+def get_all_numbers(max_volume_n = 5, max_volume_m = pi):
+	list_of_m_volumes = [0,max_volume_m]
+	list_of_n_volumes = [0, max_volume_n]
 	end = False
 	not_calculated = 0
 	count = 0
 	while end == False: 
-		n = 0
-		n_ = volume_n
-		m = 0
-		m_ = volume_m	
+		momentary_volume_n_from_empty = 0
+		momentary_volume_n_from_full = max_volume_n
+		momentary_volume_m_from_empty = 0
+		momentary_volume_m_from_full = max_volume_m	
 		# n -m*k
-		while n_ > 0 and count ==0 :
-			if n_ not in list_n:
-				list_n.append(n_)
-			n_ -= volume_m
+		while momentary_volume_n_from_full > 0 and count == 0 :
+			if momentary_volume_n_from_full not in list_of_n_volumes:
+				list_of_n_volumes.append(momentary_volume_n_from_full)
+			momentary_volume_n_from_full -= max_volume_m
 		# m*k
-		while (n + volume_m)< volume_n :
+		while (momentary_volume_n_from_empty + max_volume_m)< max_volume_n :
 			if not_calculated != 0:
-				if not_calculated not in list_n:
-					list_n.append(not_calculated)
-				n += not_calculated
+				if not_calculated not in list_of_n_volumes :
+					list_of_n_volumes .append(not_calculated)
+				momentary_volume_n_from_empty += not_calculated
 				not_calculated = 0	
 			else:	
-				if n not in list_n:
-					list_n.append(n)
-				n += volume_m 
-		list_n.append(n)		
-		m_num = volume_m - (volume_n - n)
-		m_num2 = list_n[1]
-		if m_num in list_m:
+				if momentary_volume_n_from_empty not in list_of_n_volumes :
+					list_of_n_volumes .append(momentary_volume_n_from_empty)
+				momentary_volume_n_from_empty += max_volume_m 
+		list_of_n_volumes.append(momentary_volume_n_from_empty)	
+
+		residue_of_container_m_from_cyle_n_empty_to_full = max_volume_m - (max_volume_n - momentary_volume_n_from_empty)
+
+		if residue_of_container_m_from_cyle_n_empty_to_full in list_of_m_volumes :
 			pass
 		else:
-			list_m.append(m_num)
-		if count >10 or m_num2 in list_m :
-			pass
-		elif m_num2 != volume_n:		
-			list_m.append(m_num2)
+			list_of_m_volumes.append(residue_of_container_m_from_cyle_n_empty_to_full)
+
 		end = True
 
-		if m_num not in list_n:
-			list_n.append(m_num)
-			not_calculated = m_num
+		if residue_of_container_m_from_cyle_n_empty_to_full not in list_of_n_volumes:
+			list_of_n_volumes.append(residue_of_container_m_from_cyle_n_empty_to_full)
+			not_calculated = residue_of_container_m_from_cyle_n_empty_to_full
 			end = False	
 		count += 1
 		if count > 1000: # Stopp der Kalkulation 
 			break
-	list_m.sort()
-	list_n.sort()
-	print("Every n:  ", list_n,"'\n"+"'\n"+"Every m:  ", list_m)
+	list_of_m_volumes.sort()
+	list_of_n_volumes.sort()
+	print("\n \n \n \n" +"Nearly every possible volume of container n:  ", list_of_n_volumes,"\n \n \n \n"+"Nearly every possible volume of container m:  ", list_of_m_volumes)
 run = get_all_numbers() 
 		 
-	
